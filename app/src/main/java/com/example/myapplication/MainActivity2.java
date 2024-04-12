2d0c6df8368f4dbe802135e4cdc797f7a645fca5
Screaamer <156698070+Screaamer@users.noreply.github.com>
2024-04-12 17:07:25 +0300

    b

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -5,19 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-    private Button button2;
+    private LinearLayout layout5;
+    private LinearLayout layoutb1;
+    private LinearLayout layoutb2;
+    private LinearLayout layoutb3;
+    private LinearLayout layoutb4;
+    private LinearLayout layoutb5;
     private Button button4;
     private CalendarView calendarView1;
     private  int Date_Year;
     private  int Date_Month;
     private  int Date_DayOfMonth;
+    private TextView TextView7;
+    private int flag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +36,73 @@ public class MainActivity2 extends AppCompatActivity {
         Window window = getWindow();
         window.setStatusBarColor(Color.parseColor("#FFFFFFFF"));
         window.setStatusBarColor(Color.parseColor("#FF000000"));
-        button2 = findViewById(R.id.button2);
+
         button4 = findViewById(R.id.button4);
         calendarView1 = findViewById(R.id.calendarView1);
+        layout5 = findViewById(R.id.layout5);
+        layoutb1 = findViewById(R.id.layoutb1);
+        layoutb2 = findViewById(R.id.layoutb2);
+        layoutb3 = findViewById(R.id.layoutb3);
+        layoutb4 = findViewById(R.id.layoutb4);
+        layoutb5 = findViewById(R.id.layoutb5);
+        TextView7 = findViewById(R.id.textView7);
         calendarView1.setVisibility(View.GONE);
-        button2.setOnClickListener(new View.OnClickListener() {
+        layoutb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutb1.setBackground(getResources().getDrawable(R.drawable.button_background3));
+                layoutb2.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb3.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb4.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb5.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                flag = 1;
+            }
+        });
+        layoutb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutb1.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb2.setBackground(getResources().getDrawable(R.drawable.button_background3));
+                layoutb3.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb4.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb5.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                flag = 2;
+            }
+        });
+        layoutb3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutb1.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb2.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb3.setBackground(getResources().getDrawable(R.drawable.button_background3));
+                layoutb4.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb5.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                flag = 3;
+            }
+        });
+        layoutb4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutb1.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb2.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb3.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb4.setBackground(getResources().getDrawable(R.drawable.button_background3));
+                layoutb5.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                flag = 4;
+            }
+        });
+        layoutb5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layoutb1.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb2.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb3.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb4.setBackground(getResources().getDrawable(R.drawable.button_background2));
+                layoutb5.setBackground(getResources().getDrawable(R.drawable.button_background3));
+                flag = 5;
+            }
+        });
+        layout5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (calendarView1.getVisibility() == View.VISIBLE) {
@@ -39,19 +112,12 @@ public class MainActivity2 extends AppCompatActivity {
                 }
             }
         });
+
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
-            }
-        });
-        calendarView1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                Date_Year = i;
-                Date_Month = i1;
-                Date_DayOfMonth = i2;
             }
         });
     }
